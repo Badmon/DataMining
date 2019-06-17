bbb3146906ef8a04ae955394b4d2794027e68df3
Badmon <jleonh@autonoma.edu.pe>
2019-06-17 01:31:26 -0500

    Commit

---
+++ taller1.R
@@ -0,0 +1,11 @@
+setwd("C:/Users/user/Desktop/Mineria de Datos")
+getwd()
+
+target<-read.csv("gender_submission.csv", sep=",")
+test<-read.csv("test.csv", sep=",")
+train<-read.csv("train.csv", sep=",")
+
+
+
+
+
