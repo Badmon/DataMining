5540a7678f943d79a489108d815bb6ad105cb672
Badmon <jleonh@autonoma.edu.pe>
2019-06-17 01:32:29 -0500

    Commit

--- taller1.R
+++ taller1.R
@@ -5,7 +5,20 @@ target<-read.csv("gender_submission.csv", sep=",")
 test<-read.csv("test.csv", sep=",")
 train<-read.csv("train.csv", sep=",")
 
+###CUANDO HAY TARGET
+head(target)
+head(test)
+head(train)
 
+str(train)
+summary(train)
+
+
+testnew<-cbind(test,target)
+head(testnew)
+testnew$PassengerId<-NULL
+titan<-rbind(train,testnew)
+head(titan)
 
 
 
