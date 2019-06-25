4d1172aacb0cfe0d47e162dae43554bef4c573f1
Badmon <jleonh@autonoma.edu.pe>
2019-06-24 22:04:58 -0500

    Commit

---
+++ empresa.R
@@ -0,0 +1,9 @@
+#La Data
+empresa<-read.csv("https://raw.githubusercontent.com/VictorGuevaraP/Mineria-de-datos/master/Perdida%20de%20clientes.csv",sep = ";")
+
+#Los 6 primeros datos
+head(empresa)
+
+#En el conjunto de datos tenemos 3333 observaciones con 6 variables la variable principal es desafiliado
+str(empresa)
+
