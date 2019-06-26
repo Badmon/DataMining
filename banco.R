dd959b795ba9e6ed12704b68de7353bb85478bbc
Badmon <jleonh@autonoma.edu.pe>
2019-06-26 12:28:21 -0500

    Commit

--- banco.R
+++ banco.R
@@ -11,7 +11,6 @@ str(banco)#Cambiamos el tipo de dato del target
 banco$suscrito<-as.factor(banco$suscrito)
 summary(banco)
 
-
 ############################################################
 #VERIFICACIÓN DE VALORES PERDIDOS
 
@@ -41,6 +40,64 @@ missingraf2<- aggr(banco, col=mdc(1:2), numbers=T, sortVars=T,
                    ylab=c("Proporción de valores perdidos","Patrón de valores missing"))
 #diagrama de márgenes
 marginplot(banco[,c("duracion", "Nueva_cuenta")])
-marginplot(banco[, c("duracion", "Nueva_cuenta")], cex.numbers = 1, pch =15)
+marginplot(banco[, c("duracion", "Nueva_cuenta")], cex.numbers = 1, pch =19)
 marginplot(banco[, c("duracion", "empleo")], cex.numbers = 1, pch =15)
 marginplot(banco[, c("Nueva_cuenta", "empleo")], cex.numbers = 1, pch =15)
+
+
+#representación matricial missing
+matrixplot(banco)
+matrixplot(banco, interactive = TRUE)
+
+#Otra forma de representación
+library(mice)
+md.pattern(banco,rotate.names = T)
+
+library(Amelia)
+missmap(banco)
+
+#Visualización diferenciando tipo de dato
+library(visdat)
+vis_dat(banco)
+vis_miss(banco)
+vis_miss(banco, 
+         cluster = TRUE)
+vis_miss(banco,
+         sort_miss = TRUE)
+
+library(naniar)
+gg_miss_var(banco)
+
+##################################################################
+#Correción de missing
+
+#Eliminado todos los valores missing
+banco1<- na.omit(banc)
+library(VIM)
+aggr(banco1)
+
+#################################################################
+#Aplicando técnicas de imputación
+#Imputación por medidas de tendencia central
+library(DMwR)
+bancoc<-centralImputation(banco) #DMwR, mediana (númerico), moda(no númerico)
+summary(bancoc)
+library(VIM)
+bancoc2<-initialise(banco,method="median") #media (continuos) mediana (discretos), moda(no númerico)
+summary(bancoc2)
+
+# K-Vecinos más cercanos
+bancoknn<-knnImputation(banco, k=10)
+summary(bancoknn)
+
+#Metodos no paramétricos
+library(missForest)
+bancomiss<-missForest(banco)
+summary(bancomiss)
+head(bancomiss$ximp)
+
+
+#Metodos de regresión
+# IRMI
+bancoirmi<- irmi(banco[,1:7])
+summary(bancoirmi)
