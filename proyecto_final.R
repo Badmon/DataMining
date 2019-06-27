d8c8512b73dc33a041b26e5b6cfc39bc9714c619
Badmon <jleonh@autonoma.edu.pe>
2019-06-27 01:25:38 -0500

    Commit

--- proyecto_final.R
+++ proyecto_final.R
@@ -8,3 +8,17 @@ juegosOlimpicos<-juegosOlimpicos[,3:11]
 summary(juegosOlimpicos)
 str(juegosOlimpicos)
 
+#TRANSFORMAMOS LA DATA
+juegosOlimpicos$nationality<-as.numeric(juegosOlimpicos$nationality)
+juegosOlimpicos$sex<-as.numeric(juegosOlimpicos$sex)
+juegosOlimpicos$sport<-as.numeric(juegosOlimpicos$sport)
+juegosOlimpicos$dob<-as.Date(juegosOlimpicos$dob,"%d/%m/%Y")
+str(juegosOlimpicos)
+
+#
+which(colSums(is.na(juegosOlimpicos))!=0)
+
+colperd=c(6,7)
+
+
+
