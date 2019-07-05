f47874c9cab368bd3b0a9e3d9bfbfeb1a45435be
Badmon <jleonh@autonoma.edu.pe>
2019-07-04 19:41:42 -0500

    Commit

---
+++ Diabetes.R
@@ -0,0 +1,30 @@
+#E4
+#DATA
+data<-read.csv("https://raw.githubusercontent.com/VictorGuevaraP/Mineria-de-datos/master/E4_FILA_B.csv",sep=";")
+
+#1) COMPRENSIÓN DE LOS DATOS
+#- SALIDA 0=No 1=Si / Diabetico
+
+#2) EXPLORACION DE LOS DATOS
+#-
+head(data)
+str(data)
+summary(data)
+
+#3) VERIFICACIÓN DE LA DATA
+#Verificamos las columnas que tienen valores perdidos
+which(colSums(is.na(data))!=0)
+
+a<-which(rowSums(is.na(data))!=0)
+length(a)*100/dim(data)[1]
+
+library(VIM)
+missingraf=aggr(data,numbers=T)
+
+
+#SELECCIONAMOS LA DATA
+str(data)
+
+
+table(data$Salida)
+
